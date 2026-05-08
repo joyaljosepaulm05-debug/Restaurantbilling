aa86e10a12569b68093656d44809c0b3e66f5201
JOYAL <joyaljosepaulm05@gmail.com>
2026-05-08 12:04:32 +0530

    Cleanup: Removed unnecessary Django and .vscode folders

--- Django/hello_world/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Inventory
-
-class InventoryForm(forms.ModelForm):
-    class Meta:
-        model = Inventory
-        fields = ['item', 'quantity', 'supplier_name']
--- Django/my_site/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include # Import 'include'
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')), # Restores Login/Logout
-    path('', include('hello_world.urls')), # Connects your app's urls.py
-]
